6a35c1e5a87e812e13152cc5b150075884f25784
JuanCarlosGP <64562942+JuanCarlosGP@users.noreply.github.com>
2024-07-22 02:53:08 +0200

    xd

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactNode } from 'react';
+import { createWeb3Modal } from '@web3modal/wagmi/react';
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
+import { WagmiProvider } from 'wagmi';
+import { arbitrum, mainnet } from 'wagmi/chains';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+// Configurar el cliente de consulta
+const queryClient = new QueryClient();
+
+// Obtener el ID del proyecto desde WalletConnect Cloud
+const projectId = '6fcecab0f94daeab19ee32b879fc1440';
+
+// Crear la configuración de Wagmi
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+};
+const chains = [mainnet, arbitrum] as const;
+const config = defaultWagmiConfig({ chains, projectId, metadata });
+
+// Crear el modal de Web3
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true,
+  enableOnramp: true
+});
+
+// Proveer el contexto de Wagmi en tu aplicación
+export function Web3ModalProvider({ children }: { children: ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+// Componente principal de la aplicación
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Web3ModalProvider>
+      <div className="App">
+        <h1>🥵🥵</h1>
+        <w3m-button />
+      </div>
+    </Web3ModalProvider>
   );
 }
 
